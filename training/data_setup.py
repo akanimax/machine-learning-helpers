fc2ff18f4b3ae145361fe8366bcf01e5711d2f92
akanimax <animesh.karnewar@mobiliya.com>
2017-10-11 11:53:43 +0530

    module for setting up the training data

---
+++ training/data_setup.py
@@ -0,0 +1,8 @@
+import numpy as np # The only dependency for this module
+
+'''
+    This module includes a small set of utilities for setting up the Train, Dev and Test sets from the given raw_data.
+    This module also includes functions for synchronous random shuffling of the raw_data.
+'''
+
+# coded by Animesh Karnewar
